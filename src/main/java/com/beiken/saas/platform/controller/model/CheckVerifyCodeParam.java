83d449beb4879a8d618989df17fbb274a8cd3b31
MSI\MSI-NB <xiaobeixin50@163.com>
2021-03-27 14:09:29 +0800

    'addcommet'

--- src/main/java/com/beiken/saas/platform/controller/model/CheckVerifyCodeParam.java
+++ src/main/java/com/beiken/saas/platform/controller/model/CheckVerifyCodeParam.java
@@ -6,10 +6,10 @@ import lombok.Data;
 @Data
 public class CheckVerifyCodeParam {
     @ApiModelProperty(name = "userId", value = "用户的唯一id"
-            , required = true, example = "0")
+            , required = true, example = "1001")
     private Long userId;
     @ApiModelProperty(name = "verifyCode", value = "用户收到的验证码"
-            , required = true, example = "0")
+            , required = true, example = "123456")
     private String verifyCode;
 
 }
