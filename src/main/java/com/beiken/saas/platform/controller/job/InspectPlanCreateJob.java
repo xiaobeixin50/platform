489050b8b9258634478252127dc10738bd8eb445
boliang.pan <boliang.pan@qunar.com>
2021-05-18 23:33:35 +0800

    job

--- src/main/java/com/beiken/saas/platform/controller/job/InspectPlanCreateJob.java
+++ src/main/java/com/beiken/saas/platform/controller/job/InspectPlanCreateJob.java
@@ -28,7 +28,7 @@ public class InspectPlanCreateJob {
     private RigManager rigManager;
 
     //@Scheduled(cron = "0 15 4 ? * *")
-    @Scheduled(fixedDelay = 3600000)
+    @Scheduled(fixedDelay = 300000)
     @Transactional(rollbackFor = Exception.class)
     public void createTask() throws Exception {
         List<InspectPlanVO> inspectPlanVOs = inspectPlanManager.queryStartPlan();
